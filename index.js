e1389cf0f399a3e011ebd04c70abb36cde75ee93
niahon <niahon3107@gmail.com>
2024-11-29 14:41:40 -0300

    refactor code

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 
-
 const elTimezone = document.getElementById("timezone");
 const elTime = document.getElementById("time");
 const elDate = document.getElementById("date");
@@ -20,39 +19,75 @@ const weekArray = [
     "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 ];
 
-let hours = dayjs().hour();
-let minutes = dayjs().minute();
-let seconds = dayjs().second();
 
-let dayOfWeek = (dayjs().day());
-dayOfWeek = weekArray[dayOfWeek];
-let dayOfMonth = dayjs().date();
-let month = dayjs().month();
-month = monthsArray[month];
-let year = dayjs().year();
+window.addEventListener("load", () => {
+    let user = getData();
+    displayData(user.timezone, user.time, user.date, user.weekday);
+})
 
-// add 0 if number has only 1 digit
-if (String(seconds).length < 2) {
-    seconds = `0${seconds}`;
-}
-if (String(minutes).length < 2) {
-    minutes = `0${minutes}`;
-}
-if (String(hours).length < 2) {
-    hours = `0${hours}`;
-}
-if (String(dayOfMonth).length < 2) {
-    dayOfMonth = `0${dayOfMonth}`;
+function getData() {
+    let user = {};
+    user.timezone = getTimezone();
+    user.time = getTime();
+    user.weekday = getWeekday();
+    user.date = getDate();
+
+    return user;
 }
 
-let userTimezone = dayjs.tz.guess()
-userTimezone = userTimezone.replace("_", " ");
-userTimezone = userTimezone.replace("/", " - ");
-let userTime = `${hours}:${minutes}:${seconds}`;
-let userWeekday = dayOfWeek;
-let userDate = `${month} ${dayOfMonth}, ${year}`;
+function getTimezone() {
+    let userTimezone = dayjs.tz.guess()
+    userTimezone = userTimezone.replace("_", " ");
+    userTimezone = userTimezone.replace("/", " - ");
+    return userTimezone;
+}
 
-elTimezone.textContent = userTimezone;
-elTime.textContent = userTime;
-elDate.textContent = userDate;
-elWeekday.textContent = userWeekday;
+function getTime() {
+    let hours = dayjs().hour();
+    let minutes = dayjs().minute();
+    let seconds = dayjs().second();
+
+    if (String(seconds).length < 2) {
+        seconds = `0${seconds}`;
+    }
+    if (String(minutes).length < 2) {
+        minutes = `0${minutes}`;
+    }
+    if (String(hours).length < 2) {
+        hours = `0${hours}`;
+    }
+
+    let userTime = `${hours}:${minutes}:${seconds}`;
+    return userTime
+}
+
+function getWeekday() {
+    let dayOfWeek = dayjs().day();
+    dayOfWeek = weekArray[dayOfWeek];
+
+    let userWeekday = dayOfWeek;
+    return userWeekday;
+}
+
+function getDate() {
+    let dayOfMonth = dayjs().date();
+    let month = dayjs().month();
+    month = monthsArray[month];
+    let year = dayjs().year();
+    
+    // add 0 if number has only 1 digit
+   
+    if (String(dayOfMonth).length < 2) {
+        dayOfMonth = `0${dayOfMonth}`;
+    }
+
+    let userDate = `${month} ${dayOfMonth}, ${year}`;
+    return userDate;
+}
+
+function displayData(tz, time, date, weekday) {
+    elTimezone.textContent = tz;
+    elTime.textContent = time;
+    elDate.textContent = date;
+    elWeekday.textContent = weekday;
+}
